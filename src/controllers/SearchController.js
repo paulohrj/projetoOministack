9100e0fc318fc67d616741764e90b9fb4f4568f8
paulo <pauloramirezes@gmail.com>
2020-01-15 20:31:51 -0300

    commit inicial

---
+++ src/controllers/SearchController.js
@@ -0,0 +1,21 @@
+const Dev = require('../models/Dev');
+const parseStringArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+    async index (request, response){
+        
+        const {latitude, longitude, techs} =  request.query;
+        
+        const techsArray = parseStringArray(techs);
+        
+        const devs = await Dev.find({
+            techs: {
+              $in: techsArray,
+            },
+        });
+        
+        
+        4//console.log(techsArray);
+        return response.json(devs );
+    }
+}
